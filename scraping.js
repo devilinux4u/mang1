0a6ef5c47345d2019783030991d689db0d3777c7
devilinux4u <solo345r@gmail.com>
2024-02-20 17:56:11 +0545

    dockerized

--- scraping.js
+++ scraping.js
@@ -4,6 +4,12 @@ const puppeteer = require("puppeteer");
 const scrape = async (res) => {
 
     const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
         executablePath:
             process.env.NODE_ENV === 'production'
                 ? process.env.PUPPETEER_EXECUTABLE_PATH
